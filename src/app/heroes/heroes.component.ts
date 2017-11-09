24fc8a006f4f4beccce48dcdba9454b39fbb3529
Rohit Sharma <srrohitkandpal@gmail.com>
2017-11-09 14:24:26 +0530

    The Hero Editer
    I learnt so many things in this awesome step. Great :)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
   constructor() { }
 
   ngOnInit() {
